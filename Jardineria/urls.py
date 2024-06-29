acdc8000f626b0e8dc2e7f9c04969c5a5a15bb5c
Ayastian <se.ayalas@duocuc.cl>
2024-06-29 16:52:21 -0400

    main homepage is now homepage.html

--- Jardineria/urls.py
+++ Jardineria/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from appJardineria import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('appJardineria/', include('appJardineria.urls')),
 ]
 
