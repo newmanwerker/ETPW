7c41ce3dcdb311e2ca1a83137c64c3efae2e2002
Ayastian <se.ayalas@duocuc.cl>
2024-07-12 22:56:50 -0400

    login y registrarse añadido completamente, tarea: modificar estilos de usuario activo en sesión

--- appJardineria/urls.py
+++ appJardineria/urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('nosotros', views.nosotros, name='nosotros'),
     path('home', views.home, name='home'),
     path('checkout', views.checkout, name='checkout'),
-    path('login', views.login, name='login'),
+    path('loginPage', views.loginPage, name='loginPage'),
     path('registro', views.registro, name='registro'),
+    path('logoutUser', views.logoutUser, name='logoutUser'),
     #CRUD DE PRODUCTOS
    path('productos_list', views.productos_list, name='productos_list'),
    path('productosAdd', views.productosAdd, name='productosAdd'),
